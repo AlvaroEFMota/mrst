5952f3bd7d4b50064f0f10a95cbbe9409b1adcac
Alvaro E. F. Mota <alvaro11318@gmail.com>
2022-08-10 00:46:23 -0300

    Implementing the BFS

--- main.cpp
+++ main.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-struct GraphContainer{
+struct GraphContainer
+{
     vector<vector<int>> graph;
-    std::pair<int,int> matching;
+    std::pair<int, int> matching;
     int len;
     bool *part;
 };
 
 GraphContainer GraphInit();
 void ShowGraph(GraphContainer G, string graph_desc);
+void CalculateBipartite(GraphContainer G);
+void BFS(GraphContainer G, int source, bool *setted);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     GraphContainer G = GraphInit();
 }
 
-GraphContainer GraphInit(){
+GraphContainer GraphInit()
+{
     GraphContainer G;
     int num_vertex, num_edges;
     cin >> num_vertex >> num_edges;
 
-    //Constucting the conteiner graph
+    // Constucting the conteiner graph
     G.len = num_vertex;
     G.graph.resize(G.len);
-    G.part = (bool*) malloc(sizeof(bool)*G.len);
-    for (int i = 0; i < G.len; ++i) {
+    G.part = (bool *)malloc(sizeof(bool) * G.len);
+    for (int i = 0; i < G.len; ++i)
+    {
         G.part[i] = false;
     }
 
-    for (int i =0; i < num_edges; ++i) {
+    for (int i = 0; i < num_edges; ++i)
+    {
         int vertex1, vertex2;
         cin >> vertex1 >> vertex2;
         G.graph[vertex1].push_back(vertex2);
@@ -39,18 +47,78 @@ GraphContainer GraphInit(){
 
     ShowGraph(G, "Initial Graph");
 
+    CalculateBipartite(G);
+
     return G;
 }
 
-void ShowGraph(GraphContainer G, string graph_desc) {
+void ShowGraph(GraphContainer G, string graph_desc)
+{
     int count = 0;
     cout << "----- " << graph_desc << " -----" << endl;
-    for (vector<vector<int>>::iterator i = G.graph.begin(); i != G.graph.end(); ++i){
+    for (vector<vector<int>>::iterator i = G.graph.begin(); i != G.graph.end(); ++i)
+    {
         cout << count++ << ": ";
-        for (vector<int>::iterator j = (*i).begin(); j != (*i).end(); ++j){
+        for (vector<int>::iterator j = (*i).begin(); j != (*i).end(); ++j)
+        {
             cout << *j << " ";
         }
         cout << endl;
     }
     cout << "--------------------" << endl;
+}
+
+void CalculateBipartite(GraphContainer G)
+{
+    // setted is a vector thar store information about if the partition of a vertex was setted.
+    bool *setted = (bool *)malloc(sizeof(bool) * G.len);
+    for (int i = 0; i < G.len; ++i)
+    {
+        setted[i] = false;
+    }
+    cout << "sei la pra que serve: " << *setted << endl;
+
+    /*for (int source = 0; source < G.len; ++source)
+    {
+        if (setted[source] == false)
+        {
+            //BFS(G, source, setted);
+        }
+    }*/
+    BFS(G, 0, setted);
+
+    free(setted);
+}
+
+void BFS(GraphContainer G, int source, bool *setted)
+{
+    queue<int> my_queue;
+    // 0 = white, 1 = gray, 2 = black
+    int *color = (int *)malloc(sizeof(int) * G.len);
+    for (int i = 0; i < G.len; ++i)
+    {
+        color[i] = 0; // white
+    }
+
+    color[source] = 1;
+    my_queue.push(source);
+
+    while (!my_queue.empty())
+    {
+        int vertex = my_queue.front();
+        cout << "passando por " << vertex << endl;
+        my_queue.pop();
+        color[vertex] = 2; // black
+        for (vector<int>::iterator it = G.graph[vertex].begin(); it != G.graph[vertex].end(); ++it)
+        {
+            if (color[*it] == 0)
+            {
+                cout << "Adicionando o vertice " << *it << " na fila" << endl;
+                my_queue.push(*it);
+                color[*it] = 1; // gray
+            }
+        }
+    }
+
+    free(color);
 }
