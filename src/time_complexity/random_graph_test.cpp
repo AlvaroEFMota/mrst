8f574189091ec1e2732a95650265fc5170cb61b1
Álvaro Mota <alvaroefmota@gmail.com>
2023-01-24 19:33:56 -0300

    Added function to generate random graphs faster than the previous

--- src/time_complexity/random_graph_test.cpp
+++ src/time_complexity/random_graph_test.cpp
@@ -12,14 +12,14 @@ using namespace std;
 GraphContainer generate_random_graph(int n_vert, int n_edge) {
     GraphContainer random_graph(n_vert);
 
-    vector<pair<int, int> > all_possible_edges;
+    /*vector<pair<int, int> > all_possible_edges;
     for (int i = 0; i < n_vert; ++i) {
         for (int j = i+1; j < n_vert; ++j) {
             all_possible_edges.push_back(make_pair(i, j));
         }
-    }
+    }*/
 
-    //Shuffle edges
+    /*//Shuffle edges
     for (int i = n_vert-1; i >= 0; --i) {
         int drawn_index = rand()%n_vert;
         // Swaping the indexes contents
@@ -37,7 +37,27 @@ GraphContainer generate_random_graph(int n_vert, int n_edge) {
         for (int i = 0; i < all_possible_edges.size(); ++i) {
             random_graph.AddEdge(all_possible_edges[i].first, all_possible_edges[i].second);
         }
+    }*/
+
+    int maximum_attempts = int(float(n_edge)*0.05);
+    int egde_count = 0;
+    while (egde_count < n_edge) {
+        int drawn_vertex1 = rand()%n_vert;
+        int drawn_vertex2= rand()%n_vert;
+
+        int attempts = 0;
+        while (attempts < maximum_attempts) {
+            if(!random_graph.ExistEdge(drawn_vertex1, drawn_vertex2)) {
+                random_graph.AddEdge(drawn_vertex1, drawn_vertex2);
+                ++egde_count;
+                break;
+           }
+           drawn_vertex2 = (drawn_vertex2+1)%n_vert;
+           ++attempts;
+        }
     }
+
+
 
     return random_graph;
 }
@@ -45,6 +65,7 @@ GraphContainer generate_random_graph(int n_vert, int n_edge) {
 int main() {
 
     srand((unsigned)time(NULL));
+    
     /*int pfaffian_counter = 0;
     for (int i = 565; 2*i <= MAX_NUM_VERTICES; ++i) {
         int new_i = 2*i;
@@ -81,7 +102,7 @@ int main() {
         }
     }*/
     
-    int num_vertex = 12000;
+    /*int num_vertex = 12000;
     for (int edge = 0; edge < 100000000/128; ++edge) {
         int new_edge = edge*128;
         GraphContainer G = generate_random_graph(num_vertex, new_edge);
@@ -90,6 +111,20 @@ int main() {
             float interval_time = float( clock () - begin_time ) /  CLOCKS_PER_SEC;
             cout << num_vertex<<"\t"<<new_edge<< "\t" <<fixed<< interval_time<< endl;
         }
+    }*/
+
+
+    for (int vertex = 1 ; vertex < 100000000/128; ++vertex) {
+        /*m=1,5n m=2n m=4n m=10n m=n^(1,5) m=0,1*n^2*/
+        int new_vertex = vertex*128;
+        int edge = int(pow(new_vertex, 1.5));
+        // int edge = int(0.1*pow(new_vertex, 2));
+        GraphContainer G = generate_random_graph(new_vertex, edge);
+
+        const clock_t begin_time = clock();
+        BipartitePfaffianVerification(G);
+        float interval_time = float( clock () - begin_time ) /  CLOCKS_PER_SEC;
+        cout << new_vertex<<"\t"<<edge<< "\t" <<fixed<< interval_time<< endl;
     }
     
     return 0;
